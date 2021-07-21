2ab6a2f1e1b09587a33743dbf5c049669e86ff56
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-21 18:32:20 +0530

    Added styles and dynamic api fetching

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,18 +1,38 @@
 <template>
-<div>
-   <NuxtLogo />
-   <h1 class="title">Dynamic Page </h1>
-  <NuxtLink to="/">Back to home</NuxtLink>
-  <pre>{{data}}</pre>
-</div>
- 
+  <div class="container">
+    <img :src="planet.image" alt="" />
+    <h1 class="title">{{ planet.title }}</h1>
+    <NuxtLink to="/">Back to home</NuxtLink>
+    <!-- <pre>{{ $data }}</pre> -->
+  </div>
 </template>
 
 <script>
 export default {
-    async asyncData() {
-        const planet = await fetch('https://api.nuxtjs.dev/planets').then((res) => res.json())
-        return { planet }
-}
+  async asyncData({params}) {
+    const planet = await fetch(`https://api.nuxtjs.dev/planets/${params.slug}`).then((res) =>
+      res.json()
+    )
+    return { planet }
+  },
 }
 </script>
+
+<style>
+img {
+    height: 80px;
+    width: auto;
+    
+    /* object-fit: cover; */
+    }
+.container{
+    margin-top: 100px;
+
+    /* margin-bottom: 50%; */
+    min-height: 100vh;
+    text-align: center;
+}
+.title {
+    font-family: 'Courier New', Courier, monospace;
+}
+</style>
